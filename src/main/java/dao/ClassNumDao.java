20fd8422d45f5013fb8df308317558983330f09f
Kotarou-Tanoue <kmm2559381@stu.o-hara.ac.jp>
2026-04-28 12:30:19 +0900

    Dao完了

--- src/main/java/dao/ClassNumDao.java
+++ src/main/java/dao/ClassNumDao.java
@@ -12,12 +12,12 @@ import bean.School;
 
 public class ClassNumDao extends Dao {
 	// ClassNumを1件取得するメソッド
-	public ClassNum get(String class_num, School school) 
+	public ClassNum get(String class_num, School school)
 			throws Exception {
 		ClassNum classNum = new ClassNum();
 		Connection connection = getConnection();
 		PreparedStatement statement = null;
-		
+
 		try {
 			statement = connection.prepareStatement(
 					"select * from class_num where class_num =? and school_cd = ?");
@@ -25,7 +25,7 @@ public class ClassNumDao extends Dao {
 			statement.setString(2, school.getCd());
 			ResultSet rSet = statement.executeQuery();
 			SchoolDao sDao = new SchoolDao();
-			
+
 			if (rSet.next()) {
 				classNum.setClass_num(rSet.getString("class_num"));
 				classNum.setSchool(sDao.get(rSet.getString("school_cd")));
@@ -42,7 +42,7 @@ public class ClassNumDao extends Dao {
 					throw sqle;
 				}
 			}
-			
+
 			if (connection != null) {
 				try {
 					connection.close();
@@ -51,25 +51,25 @@ public class ClassNumDao extends Dao {
 				}
 			}
 		}
-		
+
 		return classNum;
-		
+
 	}
-	
+
 	// 指定された学校のクラス番号一覧を取得するメソッド
-	public List<String> filter(School school) 
+	public List<String> filter(School school)
 			throws Exception {
 		List<String> list = new ArrayList<>();
 		Connection connection = getConnection();
 		PreparedStatement statement = null;
-		
+
 		try {
-			
+
 			statement = connection.prepareStatement(
 					"select class_num from class_num where school_cd=? order by class_num");
 			statement.setString(1,school.getCd());
 			ResultSet rSet = statement.executeQuery();
-			
+
 			while (rSet.next()) {
 				list.add(rSet.getString("class_num"));
 			}
@@ -83,7 +83,7 @@ public class ClassNumDao extends Dao {
 					throw sqle;
 				}
 			}
-			
+
 			if (connection != null) {
 				try {
 					connection.close();
@@ -92,17 +92,86 @@ public class ClassNumDao extends Dao {
 				}
 			}
 		}
-		
+
 		return list;
 	}
-	
-	
-//	public boolean save(ClassNum classNum) 
-//	throws Exception {
-//}
-//
-//
-//public boolean save(ClassNum classNum, String newClassNum)
-//	throws Exception {
-//}
+
+	/*
+	 * saveメソッド: クラスの追加をする
+	 * 戻り値: boolean型 trueなら追加成功
+	 * */
+	public boolean save(ClassNum classNum) throws Exception {
+		Connection connection = getConnection();
+		PreparedStatement statement = null;
+		int count = 0;
+
+		try {
+			statement = connection.prepareStatement(
+					"insert into class_num(school_cd, class_num) values(?, ?) ");
+			statement.setString(1, classNum.getSchool().getCd());
+			statement.setString(2, classNum.getClass_num());
+
+			count = statement.executeUpdate();
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+		}
+
+		return count > 0;
+	}
+
+	/*
+	 * saveメソッド: クラスの変更を行う
+	 * 戻り値: boolean型 trueなら変更成功
+	 * */
+	public boolean save(ClassNum classNum, String newClassNum) throws Exception {
+		Connection connection = getConnection();
+		PreparedStatement statement = null;
+		int count = 0;
+
+		try {
+			statement = connection.prepareStatement(
+					"update class_num set class_num=? where school_cd=? and class_num=?");
+			statement.setString(1, classNum.getClass_num());
+			statement.setString(2, classNum.getSchool().getCd());
+			statement.setString(3, newClassNum);
+
+			count = statement.executeUpdate();
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+		}
+
+		return count > 0;
+	}
 }
